5bef94c5ec2c27d669aa0e4f13c2ce121743fef8
Ayush Sharma <116947667+ayushsharma00001@users.noreply.github.com>
2024-07-06 10:57:35 +0530

    updated 1

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ app.use(express.static(path.join(__dirname,"public")));
 
 app.set("views",path.join(__dirname,"views"));
 
-app.use(express.urlencoded({__dirname:true}));
-
 app.get("/",(req,res,next)=>{
     res.render("index.ejs");
 })
